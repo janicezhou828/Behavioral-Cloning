80cb12f71194baa6bd4250fedf2fd333673493ef
Janice <45135092+janicezhou828@users.noreply.github.com>
2020-07-15 17:55:23 -0400

    Imported data
    Imported training data

--- Cloning.py
+++ Cloning.py
@@ -1,13 +1,25 @@
 import csv
+import cv2
 
 lines = []
 with open ('./Data/Round1/driving_log.csv') as csvfile:
 	reader = csv.reader(csvfile)
 	for line in reader:
 		lines.append (line)
+images = []
+measurements = []
 
 for line in lines:
 	source_path = line[0]
-	tokens = source_path.split('/')
-	print(tokens)
-	exit()
+	tokens = source_path.split('\\')
+	filename = tokens[-1]
+	local_path = "./Data/" + filename
+	image = cv2.imread(local_path)
+	images.append(image)
+	measurement = line[3]
+	measurements.append(measurement)
+	
+print (len(images))
+print (len(measurements))
+
+
