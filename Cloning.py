1927808a923be033371c71dfb826ddbd6ba2ee38
Janice <45135092+janicezhou828@users.noreply.github.com>
2020-07-15 21:33:34 -0400

    Trained and tested first model
    Trained and tested the first model (10ep) on simulator

--- Cloning.py
+++ Cloning.py
@@ -40,7 +40,7 @@ model.add(Flatten(input_shape=(160,320,3)))
 model.add(Dense(1))
 
 model.compile(optimizer='adam',loss='mse')
-model.fit(X_train,y_train,validation_split = 0.2 , shuffle = True)
+model.fit(X_train,y_train,validation_split = 0.2 , shuffle = True, epochs = 10)
 
 model.save('testmodel.h5')
 
